fcc1ad9b17d45356200143322eba269e4a6b93d7
joao vitor <joaovitormirandabandeira@gmail.com>
2023-02-16 21:34:43 -0300

    Criando a Class JogoDaVida, declarando as variaves principais, e criando a primeira interação com o usuario

---
+++ JogoDaVida.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class JogoDaVida{
+   public static void main(String[] args){
+      //Variaveis
+      Scanner leia = new Scanner(System.in);
+      int n;
+      //Variaveis
+      
+      //Declara o tamnho da Matriz
+      System.out.println("Qual tamanho da Matriz?");
+      n = leia.nextInt();
+      int[][] matriz = new int[n][n];
+      //Declara o tamnho da Matriz
+   }
+}

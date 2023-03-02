efcc77f78aeea1be037e2a44dd3b870e6d25ed81
joao vitor <joaovitormirandabandeira@gmail.com>
2023-03-02 10:00:56 -0300

    Implementando o loop no jogo, e um metodo copy para ser usado na validação do loop

--- .vscode/java.java
+++
@@ -1,4 +0,0 @@
-package .vscode;
-
-public class java {
-}
--- JogoDaVida.java
+++ JogoDaVida.java
@@ -1,9 +1,12 @@
 import java.util.*;
 import java.util.Random;
+import java.util.Arrays;
 
 public class JogoDaVida {
    static Validacao validador = new Validacao();
    static Matriz matriz = new Matriz();
+   static int[][] matrizAnterior ;
+   static int[][] newMatriz ;
 
    // Declaração do método gerarNumerosAleatorios
    public static int gerarNumerosAleatorios() {
@@ -48,25 +51,25 @@ public class JogoDaVida {
             if (validador.validacaoDasPontas(i, j, k)) {
                // Valida em qual canto esta sendo analisado
                if (validador.validaCantoSuperiorEsquerdo(contador)) {
-                  System.out.println("Estou no canto Superior Esquerdo");
+                  // System.out.println("Estou no canto Superior Esquerdo");
                   int somatorio = matriz[i][k] + matriz[i][j + 1] + matriz[k][j] + matriz[i + 1][j];
                   matriz[i][j] = regrasDeAtualizacaoDaMatriz(matriz[i][j], somatorio);
                   somatorio = 0;
                   contador++;
                } else if (validador.validaCantoSuperiorDireito(contador)) {
-                  System.out.println("Estou no canto Superior Direito");
+                  // System.out.println("Estou no canto Superior Direito");
                   int somatorio = matriz[i][j - 1] + matriz[i][0] + matriz[k][j] + matriz[i + 1][j];
                   matriz[i][j] = regrasDeAtualizacaoDaMatriz(matriz[i][j], somatorio);
                   somatorio = 0;
                   contador++;
                } else if (validador.validaCantoInferiorEsquerdo(contador)) {
-                  System.out.println("Estou no canto Inferior Esquerdo");
+                  // System.out.println("Estou no canto Inferior Esquerdo");
                   int somatorio = matriz[i][k] + matriz[i][j + 1] + matriz[0][j] + matriz[i - 1][j];
                   matriz[i][j] = regrasDeAtualizacaoDaMatriz(matriz[i][j], somatorio);
                   somatorio = 0;
                   contador++;
                } else if (validador.validaCantoInferiorDireito(contador)) {
-                  System.out.println("Estou no canto Inferior Direito");
+                  // System.out.println("Estou no canto Inferior Direito");
                   int somatorio = matriz[i][j - 1] + matriz[i][0] + matriz[0][j] + matriz[i - 1][j];
                   matriz[i][j] = regrasDeAtualizacaoDaMatriz(matriz[i][j], somatorio);
                   somatorio = 0;
@@ -99,25 +102,41 @@ public class JogoDaVida {
                int somatorio = matriz[i][j-1] + matriz[i][j+1] + matriz[i-1][j] + matriz[i+1][j];
                matriz[i][j] = regrasDeAtualizacaoDaMatriz(matriz[i][j], somatorio);
                somatorio = 0;
-               System.out.println("Estou no meio");
+               // System.out.println("Estou no meio");
             }
             // Valida onde esta sendo analisado a matriz, para fazer as tratativas coretas.
          }
       }
       return matriz;
    }
+   public static int[][] copiaMatriz(int[][] original) {
+      int[][] copy = new int[original.length][original[0].length];
+      for (int i = 0; i < original.length; i++) {
+          for (int j = 0; j < original[i].length; j++) {
+              copy[i][j] = original[i][j];
+          }
+      }
+      return copy;
+  }
 
    public static void main(String[] args) {
       // Variaveis
-      Scanner leia = new Scanner(System.in);
       matriz.definirDimencaoMatriz();
       // Variaveis
       matriz.preencherMatriz();
       // Chama o método formatarMatriz para imprimir a matriz formatada
       System.out.println(matriz.formatarMatriz());
-      System.out.println(matriz.length());
-      System.out.println(matriz.getMatriz());
-      matriz.setMatriz(jogoDaVida(matriz.getMatriz(), matriz.length()));
-      System.out.println(matriz.formatarMatriz());
+      
+do {
+   Scanner scanner = new Scanner(System.in);
+    matrizAnterior = copiaMatriz(matriz.getMatriz());
+   //  System.out.println(matriz.formatarMatriz());
+    newMatriz = jogoDaVida(matriz.getMatriz(), matriz.length());
+    matriz.setMatriz(newMatriz);
+    System.out.println(matriz.formatarMatriz());
+    System.out.println(!Arrays.deepEquals(matrizAnterior, newMatriz));
+    System.out.println("Pressione Enter para continuar...");
+    scanner.nextLine(); // aguarda a entrada do usuário
+} while (!Arrays.deepEquals(matrizAnterior, newMatriz));
    }
 }
